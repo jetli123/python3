66674184c658d5e7c78b22c79a1aba8063223ad4
jetli123 <Jet_Oracle@outlook.com>
2018-09-03 16:20:29 +0800

    second commit file for PyCharm

--- 廖雪峰-python/廖雪峰-module.py
+++ 廖雪峰-python/廖雪峰-module.py
@@ -12,13 +12,13 @@ import sys
 def test():
     args = sys.argv
     if len(args) == 1:
-        print 'This is a python file for %s ' % args[0]
+        print('This is a python file for %s ' % args[0])
     elif len(args) == 2:
-        print 'Hello, %s !' % args[1]
+        print('Hello, %s !' % args[1])
     elif len(args) == 3:
-        print 'The second and third parameters is %s, %s' % (args[1], args[2])
+        print('The second and third parameters is %s, %s' % (args[1], args[2]))
     else:
-        print 'Too many arguments!'
+        print('Too many arguments!')
 
 
 test()
@@ -29,5 +29,5 @@ __name__置为__main__，而如果在其他地方导入该 hello 模块时， if
 些额外的代码，最常见的就是运行测试'''
 
 if __name__ == '__main__':
-    print "LxFmodule.py 作为主程序运行！"
-    raw_input("Enter any str")
+    print("LxFmodule.py 作为主程序运行！")
+    input("Enter any str")
