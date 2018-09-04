e0e717902e908b5fa2406e97d767c40434861be6
jetli123 <Jet_Oracle@outlook.com>
2018-09-04 14:18:10 +0800

    second commit file for PyCharm

--- 廖雪峰-python/廖雪峰-函数参数之 命名关键字参数.py
+++ 廖雪峰-python/廖雪峰-函数参数之 命名关键字参数.py
@@ -36,11 +36,30 @@ person('Shift', 80, city='Beijing', job='Teacher')
 #     缺少 *，city 和 job 被视为位置参数
     # pass
 
+# 在 Python 中定义函数，可以用必选参数、默认参数、可变参数、关键
+# 字参数和命名关键字参数，这 5 种参数都可以组合使用，除了可变参数
+# 无法和命名关键字参数混合。但是请注意，参数定义的顺序必须是：必
+# 选参数、默认参数、可变参数/命名关键字参数和关键字参数。
 # -*- 参数组合 使用 -*-
+# 1.位置参数，默认参数，可变参数，关键字参数组合
 def f1(name, old, size=0, *args, **kw):
     print('name =', name, 'old =', old, 'size =', size, 'args =', args, 'kw =', kw)
+
+# 2.位置参数，默认参数，命名关键字参数，关键字参数组合
+def f2(name, old, size=0, *, city, **kw):
+    print('name=', name, 'old=', old, 'size=', size, 'city=', city, 'kw=', kw)
 
 dd = {'ages': 22, 'address': 'Beijing'}
 cc = (1, 2, 3, 4)
 f1('Jack', 23, 330, *cc, **dd)
+# name = Jack old = 23 size = 330 args = (1, 2, 3, 4) kw = {'ages': 22, 'address': 'Beijing'}
+f2('Jack', 23, 340, city='Beijing', **dd)
+# name= Jack old= 23 size= 340 city= Beijing kw= {'ages': 22, 'address': 'Beijing'}
+
+# 所以，对于任意函数，都可以通过类似 func(*args, **kw)的形式调用它，
+# 无论它的参数是如何定义的。
+aa = ('Jack', 100, 270)
 f1(*cc, **dd)
+# name = 1 old = 2 size = 3 args = (4,) kw = {'ages': 22, 'address': 'Beijing'}
+f2(*aa, city='Beijing', **dd)
+# name= Jack old= 100 size= 270 city= Beijing kw= {'ages': 22, 'address': 'Beijing'}
