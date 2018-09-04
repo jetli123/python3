861c68ce5c94d77d3ca2e8c62cfbfb0f084cdf5f
jetli123 <Jet_Oracle@outlook.com>
2018-09-04 15:29:42 +0800

    second commit file for PyCharm

--- 廖雪峰-python/廖雪峰-函数之 递归函数.py
+++ 廖雪峰-python/廖雪峰-函数之 递归函数.py
@@ -36,3 +36,16 @@ def tes(lis):
 
 cc = ['A', 'B', 'C']
 tes(cc)
+
+
+# 利用递归函数移动汉诺塔:
+def move(n, a, b, c):
+    if n == 1:
+        print('move', a, '-->', c)
+    else:
+        move(n - 1, a, c, b)
+        move(1, a, b, c)
+        move(n - 1, b, a, c)
+
+
+move(4, 'A', 'B', 'C')
