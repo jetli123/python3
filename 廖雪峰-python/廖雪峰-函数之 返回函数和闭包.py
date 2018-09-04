a2df593032aef09fae01c45ba2663f0f5f5f50c3
jetli123 <Jet_Oracle@outlook.com>
2018-09-04 15:48:09 +0800

    second commit file for PyCharm

--- 廖雪峰-python/廖雪峰-函数之 返回函数和闭包.py
+++ 廖雪峰-python/廖雪峰-函数之 返回函数和闭包.py
@@ -7,7 +7,8 @@ def calc_sum(*args):
     return ax
 
 d = (1, 2, 3, 5)
-print calc_sum(*d)
+print(calc_sum(*d))
+
 
 # 如果不需要立刻求和，而是在后面的代码中，根据需要再计算怎么办？可以不返回求和的结果，返回求和的函数
 def lazy_sum(*ab):
@@ -19,7 +20,8 @@ def lazy_sum(*ab):
     return sum
 
 f = lazy_sum(1, 2, 4, 5)
-print f()
+print(f())
+
 
 # 在这个例子中，我们在函数 lazy_sum 中又定义了函数 sum，并且，内部
 # 函数 sum 可以引用外部函数 lazy_sum 的参数和局部变量，当 lazy_sum 返
@@ -36,9 +38,9 @@ def count():
     return fs
 
 f1, f2, f3 = count()
-print f1()
-print f2()
-print f3()
+print(f1())
+print(f2())
+print(f3())
 
 # 返回闭包时牢记的一点就是：返回函数不要引用任何循环变量， 或者后
 # 续会发生变化的变量。
