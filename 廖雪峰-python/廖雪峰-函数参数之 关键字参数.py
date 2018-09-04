a67fca7af37543e00ff5bf5d0f9f2741f9a9d398
jetli123 <Jet_Oracle@outlook.com>
2018-09-04 12:32:19 +0800

    third commit file for PyCharm

--- 廖雪峰-python/廖雪峰-函数参数之 关键字参数.py
+++ 廖雪峰-python/廖雪峰-函数参数之 关键字参数.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-# -*- 关键字参数允许你传入 0 个或任意个含参数名的参数，-*-
-# -*- 这些关键字参数在函数内部自动组装为一个 dict。 -*-
+""""""
+# 可变参数允许你传入 0 个或任意个参数，这些可变参数在函数调用时自
+# 动组装为一个 tuple。而关键字参数允许你传入 0 个或任意个含参数名的
+# 参数，这些关键字参数在函数内部自动组装为一个 dict。请看示例：
 
 def person(name, age, **kw):
-    print 'name:', name, 'age:', age, 'other:', kw
+    print('name:', name, 'age:', age, 'other:', kw)
+
 
 person('Admin', 22)  # -*- name: Admin age: 22 other: {} -*-
 
