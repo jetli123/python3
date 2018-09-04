5dae06e2c278ff1ed1802c2bcc143aed04c2b3e7
jetli123 <Jet_Oracle@outlook.com>
2018-09-04 15:23:42 +0800

    second commit file for PyCharm

--- 廖雪峰-python/廖雪峰-函数之 递归函数.py
+++ 廖雪峰-python/廖雪峰-函数之 递归函数.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+在函数内部，可以调用其他函数。如果一个函数在内部调用自身本身，
+这个函数就是递归函数。
+"""
 def fact(n):
     if n == 1:
         return 1
     return n * fact(n-1)
 
-print fact(5)
+
+print(fact(5))
 
 # -*- 如果我们计算 fact(5)，可以根据函数定义看到计算过程如下： -*-
 #===> fact(5)
@@ -20,11 +25,14 @@ print fact(5)
 
 #-*- 判断一个对象是可迭代对象的方法是通过 collections 模块的 Iterable 类型判断： -*-
 from collections import Iterable
-print isinstance('abc', Iterable)
+
+print(isinstance('abc', Iterable))
+
 
 # -*- 内置的 enumerate 函数可以把一个 list 变成索引-元素对，这样就可以在 for 循环中同时迭代索引和元素本身 -*-
-def Tes(lis):
+def tes(lis):
     for i, value in enumerate(lis):
-        print i, value
+        print(i, value)
+
 cc = ['A', 'B', 'C']
-Tes(cc)
+tes(cc)
