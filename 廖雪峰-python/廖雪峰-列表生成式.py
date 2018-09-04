b058dfbb73148a008b2b2e8ddbd14d499584430e
jetli123 <Jet_Oracle@outlook.com>
2018-09-04 15:50:47 +0800

    second commit file for PyCharm

--- 廖雪峰-python/廖雪峰-列表生成式.py
+++ 廖雪峰-python/廖雪峰-列表生成式.py
@@ -2,32 +2,35 @@
 L = []
 for x in range(1, 10):
     L.append(x*x)
-print L
+print(L)
+
 
 # -*- 使用函数表示 -*-
 def Tes(A):
     for x in range(1, 7):
         A.append(x*x)
-    print A
+    print(A)
+
+
 B = []
 Tes(B)
 # -*- 练习使用列表生成器 -*-
-print [x * x for x in range(1, 5)]
+print([ x * x for x in range(1, 5) ])
 
 # -*- 使用两层循环 -*-
-print [m + n for m in 'ABC' for n in 'XYZ']
+print([m + n for m in 'ABC' for n in 'XYZ'])
 
 # -*- 列出当前目录所有文件和目录 -*-
 import os
-print [b for b in os.listdir('E:/')]
+print([b for b in os.listdir('E:/')])
 
 # -*- for 循环可同时使用多个变量 -*-
 d = {'x': '1', 'y': '4', 'z': '2'}
-print [k + '=' + v for k, v in d.items()]
+print([k + '=' + v for k, v in d.items()])
 
 # -*- 将所有字符串变成小写 -*-
 f = ['REDHAT', 'HP', 'Oracle', 'Cisco', 'IBM']
-print [s.lower() for s in f]
+print([s.lower() for s in f])
 
 # -*- 例子 -*-
 # -*- 期待输出: ['hello', 'world', 'apple'] -*-
