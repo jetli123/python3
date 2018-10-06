7cd987d87c9f58aabf5fc7439c19834ee371b526
jetli123 <Jet_Oracle@outlook.com>
2018-10-06 16:23:40 +0800

    first commit  file

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """"""
+import paramiko
+
+hostname = '192.168.1.213'
+username = 'hadoop'
+password = 'hadoop'
+port = 22
+
+try:
+    t = paramiko.Transport((hostname, port))
+    t.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(t)
+    sftp.put(r'E:\各种软件\CentOS-7-x86_64-DVD-1804.iso', '/ISO/CentOS-7-x86_64-DVD-1804.iso')
+    t.close()
+except Exception as e:
+    print(str(e))
