048088542431d1a0a5ff518d2826a0098b154051
jetli123 <Jet_Oracle@outlook.com>
2018-11-25 13:09:00 +0800

    second commit

--- test.py
+++ test.py
@@ -13,6 +13,12 @@ try:
     t.connect(username=username, password=password)
     sftp = paramiko.SFTPClient.from_transport(t)
     sftp.get("/root/test.txt", r"C:\Users\JetLi\Desktop\test.txt")
+    # sftp.get(r"/root/Desktop/kvm-create", r"C:\Users\JetLi\Desktop\kvm-create")  # 下载文件
+    # sftp.mkdir("/home/userdir1", 0755)  # 创建目录
+    # sftp.rmdir("/home/userdir1")  # 删除目录
+    # sftp.rename("/homile.sh")  # 文件重命名
+    # print sftp.stat("/root/sqe/test.sh", "/home/testflite3.py")  # 打印文件信息
+    # print sftp.listdir("/home")  #/ 打印目录列表
     t.close()
 except Exception as e:
     print(str(e))
