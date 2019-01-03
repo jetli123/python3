fd5c828a6868a372dbbc3e81cc649b8b2386a47f
jetli123 <Jet_Oracle@outlook.com>
2019-01-03 19:29:15 +0800

    update

--- 廖雪峰-python/廖雪峰-Json进阶.py
+++ 廖雪峰-python/廖雪峰-Json进阶.py
@@ -25,11 +25,11 @@ def student2dict(std):
 s = Student('Bob', '22', '99')
 # Student 实例首先被 student2dict()函数转换成 dict，然后再被顺
 # 利序列化为 JSON
-print json.dumps(s, default=student2dict)
+print(json.dumps(s, default=student2dict))
 
 # 不过，下次如果遇到一个 Teacher 类的实例，照样无法序列化为 JSON。
 # 我们可以偷个懒，把任意 class 的实例变为 dict：
-print json.dumps(s, default=lambda obj: obj.__dict__)
+print(json.dumps(s, default=lambda obj: obj.__dict__))
 """
     因为通常 class 的实例都有一个__dict__属性，它就是一个 dict，用来
 存储实例变量。
@@ -44,7 +44,7 @@ class Test(object):
         self.code = code
 
 d = Test('190', '10010')
-print json.dumps(d, default=lambda cuy: cuy.__dict__)
+print(json.dumps(d, default=lambda cuy: cuy.__dict__))
 
 
 # -- 我们要把 JSON 反序列化为一个 Student 对象实例，
@@ -56,7 +56,9 @@ def dict2student(f):
     return Student(f['name'], f['age'], f['score'])
 
 json_str = '{"age": "22", "score": "99", "name": "Bob"}'
-print json.loads(json_str, object_hook=dict2student)
+print(json.loads(json_str, object_hook=dict2student))
+
+
 #
 
 
@@ -65,4 +67,4 @@ def dict2student2(o):
     return Test(o['id'], o['code'])
 
 json_str1 = '{"code": "10010", "id": "190"}'
-print json.loads(json_str1, object_hook=dict2student2)
+print(json.loads(json_str1, object_hook=dict2student2))
